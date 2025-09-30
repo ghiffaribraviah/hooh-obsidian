9dea873886537f917bd02dde9f0fb49c428ed88d
ghiffaribraviah <ghiffari.bravia.hisham@gmail.com>
2025-09-30 10:19:53 +0700

    Quartz sync: Sep 30, 2025, 10:19 AM

---
+++ content/templates/Index Template.md
@@ -0,0 +1,12 @@
+---
+title:
+draft: false
+tags:
+enableToc: false
+description:
+cssclasses:
+socialImage:
+socialDescription:
+---
+ 
+The rest of your content lives here. You can use **Markdown** here :)
--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates/", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
